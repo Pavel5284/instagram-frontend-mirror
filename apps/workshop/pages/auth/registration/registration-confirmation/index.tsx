e3de2c185296ed3013f7cd9c9b88fa8e99192c8c
Margarita P <rprivalko@gmail.com>
2023-04-09 15:10:16 +0300

    feat: add Registration, Confirmation, Expired pages and services

---
+++ apps/workshop/pages/auth/registration/registration-confirmation/index.tsx
@@ -0,0 +1,21 @@
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+import RegistrationConfirmation from './Confirmation';
+import { useConfirmNewUserQuery } from '../../../../services/auth/registration/hooks';
+import { Auth } from '../../../../common/path';
+
+export default function Confirmation() {
+  const { query, push, isReady } = useRouter();
+  const { code } = query;
+  const { isLoading, isError } = useConfirmNewUserQuery(code as string);
+
+  useEffect(() => {
+    if (!code && isReady) {
+      push(Auth.Registration);
+    }
+  }, [code, isReady]);
+
+  if (isLoading) return <div style={{ color: 'white' }}>Loading...</div>;
+  if (isError) push(Auth.RegistrationExpired);
+  return !isError && <RegistrationConfirmation />;
+}
