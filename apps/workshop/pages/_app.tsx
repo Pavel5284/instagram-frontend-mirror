d68c116974cbf5eefc01fc11ef85636d453d1d93
Alex Lukomsky <lukomsky.alexander@gmail.com>
2023-04-09 12:25:56 +0300

    feat: wrapped app in layout

--- apps/workshop/pages/_app.tsx
+++ apps/workshop/pages/_app.tsx
@@ -3,6 +3,7 @@ import { createTheme, ThemeProvider } from '@mui/material/styles';
 import type { AppProps } from 'next/app';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { getScheme } from 'theme.scheme';
+import { Layout } from 'components/layout';
 
 const queryClient = new QueryClient();
 
@@ -12,7 +13,9 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
       <ThemeProvider theme={theme}>
-        <Component {...pageProps} />
+        <Layout isCreateProfilePage>
+          <Component {...pageProps} />
+        </Layout>
       </ThemeProvider>
     </QueryClientProvider>
   );
