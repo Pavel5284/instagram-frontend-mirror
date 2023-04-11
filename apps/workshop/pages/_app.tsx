bc11d3e0418de4aad1a23cbedbadf514e4e6704f
Alex Lukomsky <lukomsky.alexander@gmail.com>
2023-04-12 02:32:27 +0300

    feat: react-query-devtools and fix: duplication module eslint

--- apps/workshop/pages/_app.tsx
+++ apps/workshop/pages/_app.tsx
@@ -1,17 +1,20 @@
+import { useState } from 'react';
 import 'styles/globals.css';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import type { AppProps } from 'next/app';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { getScheme } from 'theme.scheme';
 import { Layout } from 'components/layout';
-
-const queryClient = new QueryClient();
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 const theme = createTheme(getScheme());
 
 export default function App({ Component, pageProps }: AppProps) {
+  const [queryClient] = useState(() => new QueryClient());
+
   return (
     <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
       <ThemeProvider theme={theme}>
         <Layout isCreateProfilePage>
           <Component {...pageProps} />
